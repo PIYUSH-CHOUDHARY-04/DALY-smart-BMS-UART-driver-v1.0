e22493b20bb066853b6b02f2d94a8aaf3151d8a1
Piyush Choudhary <mercmerc961@gmail.com>
2025-01-13 12:55:26 +0530

    Add files via upload

--- Inc & Src/bms_uart_comm.h
+++ Inc & Src/bms_uart_comm.h
@@ -23,8 +23,63 @@ extern "C" {
  *	 hardware architecture.
  */ 
 
+// ====================================================================================================== MACROS ==========================================================================================================================
+
+/**
+ * @brief macros for hardware address of the different modules available on BMS.
+ */
+#define BMS_MASTER_ADDR		0x01	/**< Address for the microcontroller chip inside BMS		*/
+#define GPRS_ADDR		0x20	/**< Address for the GPRS chip inside BMS			*/
+#define UPPER_CMPTR_ADDR	0x40	/**< Address for the host computer/microcontroller inside BMS	*/
+#define BLUETOOTH_APP_ADDR	0x80	/**< Address for the bluetooth chip inside BMS
+
+/**
+ *@brief macros for custom protocol packet structure for UART since UART is a serial protocol, thus its responsibility of hardware designed to choose a specific protocol packet format.
+ */
+#define MAX_DATA_SIZE 	0x08	/**< In bytes	*/
+#define START_FLAG	0xA5	/**< First byte of custom protocol packet indicating packet transmission initialization	*/
+
+/**
+ * @brief macros for "Data ID" field values.
+ */
+#define BMS_RESET			0x00
+#define SOC_TOTAL_IV			0x90
+#define MAX_MIN_VOLTAGE			0x91
+#define MAX_MIN_TEMPERATURE		0x92
+#define CHRG_DISCHRG_MOS_STATUS		0x93
+#define STATUS_INFO_1			0x94
+#define CELL_VOLTAGE			0x95
+#define CELL_TEMPERATURE		0x96
+#define CELL_BALANCE_STATE		0x97
+#define	BATTERY_FAILURE_STATUS		0x98
+#define DISCHRG_FET			0xD9
+#define CHRG_FET			0xDA
 
 
+//================================================================================================ PROTOCOL PACKET STRUCTURE ===============================================================================================================
+
+/**
+ * @brief 
+ */
+typedef struct {
+	uint8_t start_flag;			/**< This is a constant value indicating the start of the custom UART protocol packet/frame , start_flag=START_FLAG 									*/
+	uint8_t module_addr;			/**< Address of specific chip/module inside the BMS, supported values are listed above in MACRO section											*/
+	uint8_t data_id;			/**< ID of the specific data that host is querying for or want to configure BMS (may be supported in some advanced BMS(s)), supported values are listed above in MACRO section		*/
+	uint8_t data_len;			/**< Length of the data in the data field, fixed for this custom protocol implementation over UART for packet parsing, it has a constant value of MAX_DATA_SIZE for all TX/RX packets	*/
+	uint8_t data[MAX_DATA_SIZE];		/**< Memory space holding the actual data to be sent or received															*/
+	uint8_t chksum;				/**< Checksum of all the previous bytes																			*/
+} uart_prot_packet;
+
+
+
+
+
+
+
+
+/**
+ *
+ *
 
 #ifdef __cplusplus
 }
