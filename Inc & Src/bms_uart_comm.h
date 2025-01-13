19d623c648a39c4d1c3a8494252ec1c1fd17b0e6
Piyush Choudhary <mercmerc961@gmail.com>
2025-01-13 15:36:35 +0530

    Add files via upload

--- Inc & Src/bms_uart_comm.h
+++ Inc & Src/bms_uart_comm.h
@@ -23,6 +23,7 @@ extern "C" {
  *	 hardware architecture.
  */ 
 
+
 // ====================================================================================================== MACROS ==========================================================================================================================
 
 /**
@@ -55,11 +56,33 @@ extern "C" {
 #define DISCHRG_FET			0xD9
 #define CHRG_FET			0xDA
 
+/**
+ * @brief macro for default baudrate of UART interface.
+ */
+#define UART_DEFAULT_BAUDRATE		9600	/**< units : bps (bits per second)	*/
+
+/**
+ * @brief macros for selecting only the required memory regions corresponding to the specific data field saving memory.
+ */
+#define _FULL_READ_ACCESS 	0x00		/**< Need to be selected by the programmer, 0x00 means this option is disabled, 0x01 means full access is enabled */
+
+#if _FULL_READ_ACCESS == 0x00
+	/* All folloing macro can be set to either 0x00 or 0x01, if set 0x00, corresponding memory can't be accessed and thus space won't be allocated to the data for that region in RT_Battery_status structure, if set to 0x01, it can be 	   accessed to read data out of it, this is benefecial for both memory and time consumption, thus fastening the read process. */
+	#define _SOC_IV_ACCESS				0x00
+	#define _MIN_MAX_VOLT_ACCESS			0x00
+	#define	_MIN_MAX_TEMP_ACCESS			0x00
+	#define _MOS_CHRG_DISCHRG_STATUS_ACCESS		0x00
+	#define _STATUS_INFO1_ACCESS			0x00
+	#define	_CELL_VOLT_ACCESS			0x00
+	#define _CELL_TEMP_ACCESS			0x00
+	#define _CELL_BALANCE_STATE_ACCESS		0X00
+	#define _BATTERY_FAILURE_STATUS_ACCESS		0X00
+#endif
 
 //================================================================================================ PROTOCOL PACKET STRUCTURE ===============================================================================================================
 
 /**
- * @brief 
+ * @brief structure for creating the custom protocol packet to be sent to the BMS.
  */
 typedef struct {
 	uint8_t start_flag;			/**< This is a constant value indicating the start of the custom UART protocol packet/frame , start_flag=START_FLAG 									*/
@@ -70,6 +93,33 @@ typedef struct {
 	uint8_t chksum;				/**< Checksum of all the previous bytes																			*/
 } uart_prot_packet;
 
+/**
+ * @brief structure for holding data received from the BMS.
+ */
+typedef struct {
+
+#if ((_FULL_READ_ACCESS | _SOC_IV_ACCESS) == 0x01)
+	uint16_t cum_total_voltage;
+	uint16_t gath_total_voltage;
+	uint16_t current;
+	uint16_t soc;
+#endif
+
+#if ((_FULL_READ_ACCESS | _MIN_MAX_VOLT_ACCESS) == 0x01)
+	uint16_t max_cell_voltage_value;
+	uint8_t cell_count_with_min_voltage;
+	uint16_t max_cell_voltage_value;
+	uint8_t cell_count_with_min_voltage;
+#endif
+
+#if
+
+#endif
+
+} RT_Battery_status;
+
+
+//================================================================================================ ROUTINE DECLARATION =====================================================================================================================
 
 
 
